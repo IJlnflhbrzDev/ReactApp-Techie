668907d41885a74efdf9b284f70cf5db549a3591
IJlnflhbrzDev <IJlnflhbrzDev@gmail.com>
2021-01-08 00:33:56 +0700

    + animated scroll React mengunakan ComponentDidmounth"

--- src/component/Header/CHeader.jsx
+++ src/component/Header/CHeader.jsx
@@ -4,6 +4,13 @@ import '../Header/CHeader.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 export default class CHeader extends Component {
+     constructor(props) {
+          super(props);
+          this.state = {
+               img : 'https://bootstrapmade.com/demo/templates/Techie/assets/img/hero-img.png'
+          }
+     }
+
      componentDidMount() {
           AOS.init();
      }
@@ -16,13 +23,13 @@ export default class CHeader extends Component {
                                    <Col xl="5" lg="5" md="6" sm="12" className=" d-flex justify-content-center align-items-center">
                                         <div className="header-title "  data-aos="fade-up">
                                              <h1 className="text-white  font-weight-bold">Better Digital <br /> Experience With <br /> Techie</h1>
-                                             <h2>We are team of talanted designers making websites with <br/> <strong>IJlnflhbrz</strong></h2>
-                                             <Button >Get Started</Button>
+                                             <h2>We are team of talanted designers making websites with  <strong>IJlnflhbrz</strong></h2>
+                                             <Button>GET STARTED</Button>
                                         </div>
                                    </Col>
                                    <Col xl="4" lg="6" md="6" sm="12" className="d-flex justify-content-center align-items-center position-relative">
                                         <div className="header-image">
-                                             <img data-aos="zoom-in" src="https://bootstrapmade.com/demo/templates/Techie/assets/img/hero-img.png" alt="hero"/>
+                                             <img data-aos="zoom-in" src={this.state.img} alt="hero"/>
                                         </div>
                                    </Col>
                               </Row>
--- src/component/Navbar/CNavbar.jsx
+++ src/component/Navbar/CNavbar.jsx
@@ -3,19 +3,42 @@ import  "bootstrap/dist/css/bootstrap.min.css";
 import {Navbar,Nav,NavDropdown,Container } from "react-bootstrap";
 import "../Navbar/CNavbar.css"
 export default class CNavbars extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title : 'TECHIE'
+    }
+  }
+  componentDidMount() {
+    const Navbar = document.querySelector('.navbar');
+    window.onscroll = function () {
+      const topWindowScroll = window.scrollY;
+
+      if (topWindowScroll > 100) {
+          Navbar.classList.add('activ-chance');
+      }
+
+      else {
+          Navbar.classList.remove('activ-chance');
+      }
+
+    }
+
+
+  }
   render() {
     return (
-      <Navbar  expand="md" className="p-2 fixed-top bg-transparent">
+      <Navbar expand="md" className="p-2 fixed-top bg-transparent">
         <Container>
-          <Navbar.Brand href="#home">TECHIE</Navbar.Brand>
+          <Navbar.Brand href="#home">{this.state.title}</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link href="#service">Service</Nav.Link>
-                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-                <Nav.Link href="#team">Team</Nav.Link>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#service">Service</Nav.Link>
+              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="#team">Team</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -26,7 +49,7 @@ export default class CNavbars extends Component {
               <Nav.Link href="#link">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
-          </Container>
+        </Container>
       </Navbar>
     )
   }
