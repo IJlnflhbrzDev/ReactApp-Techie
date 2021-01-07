d0c9d3c053bea4367306e3b6c548695a79e6ad35
IJlnflhbrzDev <IJlnflhbrzDev@gmail.com>
2021-01-08 00:54:50 +0700

    + animated loader web load

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -3,12 +3,32 @@ import CNavbars from '../component/Navbar/CNavbar'
 import CHeader from '../component/Header/CHeader'
 
 export default class Home extends Component {
+     constructor(props) {
+          super(props);
+          this.state = {
+               loader : 'https://codetea.com/content/images/2017/10/CSS-Loader.gif'
+          }
+     }
+
+     componentDidMount() {
+     window.addEventListener('load',function () {
+          const Loader = document.querySelector('.loader');
+          Loader.style.display = "none";
+     })
+}
+
      render() {
           return (
                <>
+                    <LoaderElement />
                     <CNavbars />
                     <CHeader />
                </>
           )
      }
+}
+
+// Loaader
+const LoaderElement = () => {
+     return <div className="loader"></div>
 }
