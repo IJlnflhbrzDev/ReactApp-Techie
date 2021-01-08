9124c508f1f061349c626b8058ec0f1dc58365ea
IJlnflhbrzDev <IJlnflhbrzDev@gmail.com>
2021-01-09 00:49:56 +0700

    Latihan Pekondisian button state awal GET STARTED ketika di klik mengubah state menjadi You Started

--- src/component/Header/CHeader.jsx
+++ src/component/Header/CHeader.jsx
@@ -7,9 +7,24 @@ export default class CHeader extends Component {
      constructor(props) {
           super(props);
           this.state = {
-               img : 'https://bootstrapmade.com/demo/templates/Techie/assets/img/hero-img.png'
+               img: 'https://bootstrapmade.com/demo/templates/Techie/assets/img/hero-img.png',
+               started :'GET STARTED'
           }
      }
+
+     handleStated = () => {
+          if (this.state.started === "GET STARTED") {
+
+               this.setState({
+                    started : "You Started"
+               });
+          }else {
+               this.setState({
+                    started : 'GET STARTED'
+               })
+          }
+     }
+
 
      componentDidMount() {
           AOS.init();
@@ -24,7 +39,7 @@ export default class CHeader extends Component {
                                         <div className="header-title "  data-aos="fade-up">
                                              <h1 className="text-white  font-weight-bold">Better Digital <br /> Experience With <br /> Techie</h1>
                                              <h2>We are team of talanted designers making websites with  <strong>IJlnflhbrz</strong></h2>
-                                             <Button>GET STARTED</Button>
+                                             <Button onClick={this.handleStated}>{this.state.started}</Button>
                                         </div>
                                    </Col>
                                    <Col xl="4" lg="6" md="6" sm="12" className="d-flex justify-content-center align-items-center position-relative">
