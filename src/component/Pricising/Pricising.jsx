15bf97cda172239b3045b472349f605c16cafc5f
IJlnflhbrzDev <IJlnflhbrzDev@gmail.com>
2021-01-16 00:13:11 +0700

    +Before Clipy "

--- src/component/Pricising/Pricising.jsx
+++ src/component/Pricising/Pricising.jsx
@@ -17,8 +17,8 @@ export default function Pricising() {
                     <Row>
                          <Col xl="3" lg="4" md="6" sm="12" className="mb-3">
                               <Card>
-                                   <Card.Header className=" text-center">Featured</Card.Header>
-                                   <Card.Img variant="top" src="https://bootstrapmade.com/demo/templates/Techie/assets/img/portfolio/portfolio-details-3.jpg" />
+                                   <Card.Header className=" text-center">Software</Card.Header>
+                                   <Card.Img variant="top" src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80" />
                                    <Card.Body>
                                         <Card.Title>Special title treatment</Card.Title>
                                         <Card.Text>
@@ -30,8 +30,8 @@ export default function Pricising() {
                          </Col>
                          <Col xl="3" lg="4" md="6" sm="12" className="mb-3">
                               <Card>
-                                   <Card.Header className=" text-center">Featured</Card.Header>
-                                   <Card.Img variant="top" src="https://bootstrapmade.com/demo/templates/Techie/assets/img/portfolio/portfolio-details-1.jpg" />
+                                   <Card.Header className=" text-center">Web Development</Card.Header>
+                                   <Card.Img variant="top" src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1052&q=80" />
                                    <Card.Body>
                                         <Card.Title>Special title treatment</Card.Title>
                                         <Card.Text>
@@ -43,8 +43,8 @@ export default function Pricising() {
                          </Col>
                          <Col xl="3" lg="4" md="6" sm="12" className="mb-3">
                               <Card>
-                                   <Card.Header className=" text-center">Featured</Card.Header>
-                                   <Card.Img variant="top" src="https://bootstrapmade.com/demo/templates/Techie/assets/img/portfolio/portfolio-details-2.jpg" />
+                                   <Card.Header className=" text-center">Mobile App</Card.Header>
+                                   <Card.Img variant="top" src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
                                    <Card.Body>
                                         <Card.Title>Special title treatment</Card.Title>
                                         <Card.Text>
@@ -56,8 +56,8 @@ export default function Pricising() {
                          </Col>
                          <Col xl="3" lg="4" md="6" sm="12" className="mb-3">
                               <Card>
-                                   <Card.Header className=" text-center">Featured</Card.Header>
-                                   <Card.Img variant="top" src="https://bootstrapmade.com/demo/templates/Techie/assets/img/portfolio/portfolio-details-2.jpg" />
+                                   <Card.Header className=" text-center">Desaigner</Card.Header>
+                                   <Card.Img variant="top" src="https://images.unsplash.com/photo-1508921340878-ba53e1f016ec?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
                                    <Card.Body>
                                         <Card.Title>Special title treatment</Card.Title>
                                         <Card.Text>
